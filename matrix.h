ad1b658ced27f399afcd5227c8190f0a85625035
Joakim Børlum <joakim@roevhat.dk>
2012-12-08 23:32:28 +0100

    BEER-WARE UPDATE

--- matrix.h
+++ matrix.h
@@ -1,8 +1,12 @@
 /*
-    RELEASED UNDER BEER-LICENSE!
-    Joakim Børlum Petersen \ #roevhat
-    AAU
-*/
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * @roevhat wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * /Joakim Børlum Petersen @roevhat
+ * ----------------------------------------------------------------------------
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
